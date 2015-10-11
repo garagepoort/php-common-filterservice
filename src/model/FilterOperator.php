4899a7cc88ea8db825af25f2018edb8981d094d0
David Maes <david.s.maes@gmail.com>
2015-10-11 14:31:10 +0200

    added FilterOperator

---
+++ src/model/FilterOperator.php
@@ -0,0 +1,23 @@
+<?php
+
+class FilterOperator extends BasicEnum{
+    const EQUALS = "equals";
+    const GREATER_THAN = "greater_than";
+    const LESS_THAN = "less_than";
+    const CONTAINS = "contains";
+    const IN = "in";
+    const ENDS_WITH = "ends_with";
+    const BEGINS_WITH = "begins_with";
+
+    public static function getDatabaseOperator($operator){
+        if($operator == FilterOperator::EQUALS){
+            return "=";
+        }
+        if($operator == FilterOperator::GREATER_THAN){
+            return ">";
+        }
+        if($operator == FilterOperator::LESS_THAN){
+            return "<";
+        }
+    }
+}
