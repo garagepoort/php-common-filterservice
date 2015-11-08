db1af0e8a4efc6dd270b17d4fd53df6534bf8c02
David Maes <david.s.maes@gmail.com>
2015-11-08 11:27:21 +0100

    change for use with angular

---
+++ src/js/com.bendani.bibliomania.book.filter.text.directive.js
@@ -0,0 +1,23 @@
+angular
+    .module('com.bendani.php.common.filterservice')
+    .directive('filterText', function (){
+        return {
+            scope: {
+                filter: "=filter"
+            },
+            restrict: "E",
+            template: '<div ng-include="getTemplateUrl()"></div>',
+            controller: ['$scope', '$rootScope', function($scope, $rootScope) {
+                $scope.filter.value = "";
+                $scope.filter.selectedOperator = $scope.filter.supportedOperators[0].value;
+
+                $scope.shouldShowOperators = function(){
+                    return $scope.filter.supportedOperators.length > 1;
+                }
+
+                $scope.getTemplateUrl = function(){
+                    return $rootScope.baseUrl + "packages/bendani/php-common/filter-service/filter-boolean.html";
+                }
+            }]
+        };
+    });
