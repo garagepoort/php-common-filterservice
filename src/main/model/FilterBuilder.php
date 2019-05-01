616d620282c82674bcf15c61e5247fb11522ead2
David Maes <david.maes@kbc.be>
2019-05-01 13:57:44 +0200

    Update for elasticsearch 7

--- src/main/model/FilterBuilder.php
+++ src/main/model/FilterBuilder.php
@@ -9,53 +9,53 @@ class FilterBuilder
 {
 
     public static function wildcard($field, $wildcard){
-        return new FilterReturnType(['wildcard' => [$field => StringUtils::toLowerCase($wildcard)]]);
+        return new FilterReturnType(['must' => ['wildcard' => [$field => StringUtils::toLowerCase($wildcard)]]]);
     }
 
     public static function terms($field, $values){
         $values = self::toLowerCase($values);
 
-        return new FilterReturnType(['terms' => [$field => $values]]);
+        return new FilterReturnType(['must' => ['terms' => [$field => $values]]]);
     }
 
     public static function notTerms($field, $values){
         $values = self::toLowerCase($values);
 
-        return new FilterReturnType(['not' => ['terms' => [$field => $values]]]);
+        return new FilterReturnType(['must_not' => ['terms' => [$field => $values]]]);
     }
 
     public static function match($field, $value){
-        return new FilterReturnType(['match' => [$field => $value]]);
+        return new FilterReturnType(['must' => ['match' => [$field => $value]]]);
     }
 
     public static function term($field, $value){
-        return new FilterReturnType(['term' => [$field => $value]]);
+        return new FilterReturnType(['must' => ['term' => [$field => $value]]]);
     }
 
     public static function missing($field){
-        return new FilterReturnType(['missing' => ['field' => $field]]);
+        return new FilterReturnType(['must_not' => ['exists' => ['field' => $field]]]);
     }
 
     public static function exists($field){
-        return new FilterReturnType(['exists' => ['field' => $field]]);
+        return new FilterReturnType(['must' => ['exists' => ['field' => $field]]]);
     }
 
     public static function range($field, $greaterThan, $lessThan){
-        return new FilterReturnType(['range' => [$field => [
+        return new FilterReturnType(['must' => ['range' => [$field => [
             'gte' => $greaterThan,
             'lte' => $lessThan,
-        ]]]);
+        ]]]]);
     }
 
     public static function greaterThan($field, $value){
-        return new FilterReturnType(['range' => [$field => [
+        return new FilterReturnType(['must' => ['range' => [$field => [
             'gte' => $value
-        ]]]);
+        ]]]]);
     }
     public static function lessThan($field, $value){
-        return new FilterReturnType(['range' => [$field => [
+        return new FilterReturnType(['must' => ['range' => [$field => [
             'lte' => $value
-        ]]]);
+        ]]]]);
     }
 
     /**
